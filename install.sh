44807c6a0d55ed7603b1a1ed835fd306687bbf33
Dries De Peuter <dries@nousefreak.be>
2014-10-19 02:12:33 +0200

    Add git lg for pretty log prints

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ln -s .bash_profile ~/.bash_profile
+
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
