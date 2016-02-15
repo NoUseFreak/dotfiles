6e97313e276514a70bafcbf886de29a51234dc2d
Dries De Peuter <driesdepeuter@sparkcentral.com>
2016-02-14 17:06:06 -0800

    Add more git aliases.

--- src/git.sh
+++ src/git.sh
@@ -7,6 +7,9 @@ git config --global credential.helper osxkeychain
 git config --global core.excludesfile ~/.gitignore
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.lga "log --graph --pretty=oneline --abbrev-commit --decorate --all"
+git config --global alias.co "checkout"
+git config --global alias.ci "commit"
+git config --global alias.br "branch"
 git config --global alias.st "status -sbu"
 git config --global help.autocorrect 1
 git config --global color.ui true
