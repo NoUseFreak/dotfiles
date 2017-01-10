45b396446be42ac7ad22c1bcea507efed73e7fd8
Dries De Peuter <dries@nousefreak.be>
2017-01-10 01:40:28 +0100

    Fix theme

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,71 +1,97 @@
+# Clean, simple, compatible and meaningful.
+# Tested on Linux, Unix and Windows under ANSI colors.
+# It is recommended to use with a dark background and the font Inconsolata.
+# Colors: black, red, green, yellow, *blue, magenta, cyan, and white.
+#
+# http://ysmood.org/wp/2013/03/my-ys-terminal-theme/
+# Mar 2013 ys
 
-# heavily inspired by the wonderful pure theme
-# https://github.com/sindresorhus/pure
-
-# needed to get things like current git branch
-autoload -Uz vcs_info
-zstyle ':vcs_info:*' enable git # You can add hg too if needed: `git hg`
-zstyle ':vcs_info:git*' use-simple true
-zstyle ':vcs_info:git*' max-exports 2
-zstyle ':vcs_info:git*' formats ' %b' 'x%R'
-zstyle ':vcs_info:git*' actionformats ' %b|%a' 'x%R'
-
-autoload colors && colors
-
-git_dirty() {
-	# check if we're in a git repo
-	command git rev-parse --is-inside-work-tree &>/dev/null || return
-
-	# check if it's dirty
-	command git diff --quiet --ignore-submodules HEAD &>/dev/null;
-	if [[ $? -eq 1 ]]; then
-		echo "%F{red}✗%f"
-	else
-		echo "%F{green}✔%f"
-	fi
+# Machine name.
+function box_name {
+    [ -f ~/.box-name ] && cat ~/.box-name || echo $HOST
 }
 
-# get the status of the current branch and it's remote
-# If there are changes upstream, display a ⇣
-# If there are changes that have been committed but not yet pushed, display a ⇡
-git_arrows() {
-	# do nothing if there is no upstream configured
-	command git rev-parse --abbrev-ref @'{u}' &>/dev/null || return
+# Directory info.
+local current_dir='${PWD/#$HOME/~}'
 
-	local arrows=""
-	local status
-	arrow_status="$(command git rev-list --left-right --count HEAD...@'{u}' 2>/dev/null)"
+# VCS
+YS_VCS_PROMPT_PREFIX1=" %{$fg[white]%}on%{$reset_color%} "
+YS_VCS_PROMPT_PREFIX2=":%{$fg[cyan]%}"
+YS_VCS_PROMPT_SUFFIX="%{$reset_color%}"
+YS_VCS_PROMPT_DIRTY=" %{$fg[red]%}x"
+YS_VCS_PROMPT_CLEAN=" %{$fg[green]%}o"
 
-	# do nothing if the command failed
-	(( !$? )) || return
+# Colors
+if tput setaf 1 &> /dev/null; then
+    tput sgr0; # reset colors
+    bold=$(tput bold);
+    reset=$(tput sgr0);
+    # Solarized colors, taken from http://git.io/solarized-colors.
+    black=$(tput setaf 0);
+    blue=$(tput setaf 33);
+    cyan=$(tput setaf 37);
+    green=$(tput setaf 64);
+    orange=$(tput setaf 166);
+    purple=$(tput setaf 125);
+    red=$(tput setaf 124);
+    violet=$(tput setaf 61);
+    white=$(tput setaf 15);
+    yellow=$(tput setaf 136);
+else
+    bold='';
+    reset="\e[0m";
+    black="\e[1;30m";
+    blue="\e[1;34m";
+    cyan="\e[1;36m";
+    green="\e[1;32m";
+    orange="\e[1;33m";
+    purple="\e[1;35m";
+    red="\e[1;31m";
+    violet="\e[1;35m";
+    white="\e[1;37m";
+    yellow="\e[1;33m";
+fi;
 
-	# split on tabs
-	arrow_status=(${(ps:\t:)arrow_status})
-	local left=${arrow_status[1]} right=${arrow_status[2]}
+# Git info.
+local git_info='$(git_prompt_info)'
+ZSH_THEME_GIT_PROMPT_PREFIX="${YS_VCS_PROMPT_PREFIX1}git${YS_VCS_PROMPT_PREFIX2}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="$YS_VCS_PROMPT_SUFFIX"
+ZSH_THEME_GIT_PROMPT_DIRTY="$YS_VCS_PROMPT_DIRTY"
+ZSH_THEME_GIT_PROMPT_CLEAN="$YS_VCS_PROMPT_CLEAN"
 
-	(( ${right:-0} > 0 )) && arrows+="%F{011}⇣%f"
-	(( ${left:-0} > 0 )) && arrows+="%F{012}⇡%f"
-
-	echo $arrows
+# HG info
+local hg_info='$(ys_hg_prompt_info)'
+ys_hg_prompt_info() {
+    # make sure this is a hg dir
+    if [ -d '.hg' ]; then
+        echo -n "${YS_VCS_PROMPT_PREFIX1}hg${YS_VCS_PROMPT_PREFIX2}"
+        echo -n $(hg branch 2>/dev/null)
+        if [ -n "$(hg status 2>/dev/null)" ]; then
+            echo -n "$YS_VCS_PROMPT_DIRTY"
+        else
+            echo -n "$YS_VCS_PROMPT_CLEAN"
+        fi
+        echo -n "$YS_VCS_PROMPT_SUFFIX"
+    fi
 }
 
+# Prompt format: \n # USER at MACHINE in DIRECTORY on git:BRANCH STATE [TIME] \n $
+PROMPT="
+%{$terminfo[bold]$fg[red]%}#%{$reset_color%} \
+%{$fg[red]%}%n \
+%{$fg[white]%}in \
+%{$terminfo[bold]$fg[yellow]%}${current_dir}%{$reset_color%}\
+${hg_info}\
+${git_info}
+%{$terminfo[bold]$fg[red]%}%(?.%{$fg[green]%}.%{$fg[red]%})%B$ %{$reset_color%}"
 
-# indicate a job (for example, vim) has been backgrounded
-# If there is a job in the background, display a ✱
-suspended_jobs() {
-	local sj
-	sj=$(jobs 2>/dev/null | tail -n 1)
-	if [[ $sj == "" ]]; then
-		echo ""
-	else
-		echo "%{$FG[208]%}✱%f"
-	fi
-}
-
-precmd() {
-	vcs_info
-	print -P '\n%F{51}%~'
-}
-
-export PROMPT='%(?.%F{205}.%F{red})$%f '
-export RPROMPT='`git_dirty`%F{241}$vcs_info_msg_0_%f `git_arrows``suspended_jobs`'
+if [[ "$USER" == "root" ]]; then
+PROMPT="
+%{$terminfo[bold]$fg[blue]%}#%{$reset_color%} \
+%{$bg[yellow]%}%{$fg[cyan]%}%n%{$reset_color%} \
+%{$fg[white]%}in \
+%{$terminfo[bold]$fg[yellow]%}${current_dir}%{$reset_color%}\
+${hg_info}\
+${git_info}
+%{$terminfo[bold]$fg[red]%}%(?.%{$fg[green]%}.%{$fg[red]%})%B$ %{$reset_color%}"
+fi
