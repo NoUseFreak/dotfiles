9dcb1086e17fe89d95638b55bf35c617ba56eea5
Dries De Peuter <dries@nousefreak.be>
2017-02-09 16:23:40 +0100

    Add vim movements in zsh normal mode

---
+++ zsh/vim.zsh
@@ -0,0 +1,7 @@
+bindkey -v
+
+bindkey '^P' up-history
+bindkey '^N' down-history
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
