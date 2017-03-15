d9c3101efb39fba5efef7642648a447292ede0dc
Dries De Peuter <dries@nousefreak.be>
2017-03-14 17:56:44 -0700

    Add recursive grep

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -19,3 +19,7 @@ alias xdebug="XDEBUG_CONFIG=\"idekey=PHPSTORM\" php -d xdebug.remote_enable=true
 
 # Html2Text
 alias html2text="html2text -width $(tput cols)"
+
+# Recursive grep in folder
+alias rg="grep -rni settings/qa -e "
+
